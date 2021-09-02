0d9db8b18966d92a3eb799cd4c529f2d98424b1e
AnkitMalvi1 <88956779+AnkitMalvi1@users.noreply.github.com>
2021-09-02 22:39:11 +0530

    Add files via upload

---
+++ THA DAY3/practice_redis.sql
@@ -0,0 +1,32 @@
+//////////------------------------ THA- DAY 3 ------------- PRACTICE REDIS --------------------------//////////
+
+
+/////------------------------- [ CHAT APPLICATION ] ----------------------/////
+
+COMMAND PROMT A:
+
+Subscriber:
+
+SUBCRIBE <channel_name>
+SUBSCRIBE  devsnest
+
+
+COMMAND PROMT B:
+
+publisher:
+
+PUBLISH <channel_name> <message>
+PUBLISH  devsnest  welcome
+
+
+COMMAND PROMT A:
+
+Subscriber:
+
+(1) "message"
+(2) "devsnest"
+(3) "welcome"
+
+
+/////////--------------------MULTIPLE SUBCRIPTION ----------------------------///////
+
