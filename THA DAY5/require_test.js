7fdbe2a62fc001e6db00a7594b0f9e9ca29ffbb7
AnkitMalvi1 <88956779+AnkitMalvi1@users.noreply.github.com>
2021-09-09 17:34:53 +0530

    Add files via upload

---
+++ THA DAY5/require_test.js
@@ -0,0 +1,11 @@
+var Test = require("./crud_with_fs_module2");
+
+var obj = new Test();
+
+console.log(obj.print());
+
+
+
+// PS C:\Users\mc\Desktop\Devsnest\THA DAY5> node require_test.js
+// I'm inside require_test
+// undefined
