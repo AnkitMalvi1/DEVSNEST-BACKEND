f9f6a4f7f69795d5cdd1d4c516ef846fc40df1d5
AnkitMalvi1 <88956779+AnkitMalvi1@users.noreply.github.com>
2021-09-19 18:59:43 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use('/', (req, res)=>{
+    res
+    .status(201)
+    .cookie("token", "test", {
+        expire: new Date(Date.now() + 8 * 3600000)
+    })
+    .cookie("hello", "hello")
+    .redirect(301, "/admin")
+})
+
+
+
+// listens to a port
+app.listen(2000);
